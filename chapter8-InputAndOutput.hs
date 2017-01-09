1a184b292e61643f8c511394fd81511abaeb3eee
Patrick Becich <Patrick@Patricks-MacBook-Air.local>
2017-01-08 17:06:11 -0800

    I/O actions

--- chapter8-InputAndOutput.hs
+++ chapter8-InputAndOutput.hs
@@ -1,3 +1,29 @@
+-- main = do
+--     line <- getLine
+--     if null line
+--         then return ()
+--         else do
+--             putStrLn $ reverseWords line
+--             main
 
+-- reverseWords :: String -> String
+-- reverseWords = unwords . map reverse . words
 
-main = putStr "hello, world"
+-- main = do
+--   print True
+--   print 2
+--   print "haha"
+--   print 3.2
+--   print [3,4,2]
+
+main = do
+   a <- getLine
+   b <- getLine
+   c <- getLine
+   print [a,b,c]
+
+-- This is equivalent to:
+
+-- main = do
+--   rs <- sequence [getLine, getLine, getLine]
+--   print rs
--- helloworld.hs
+++ helloworld.hs
@@ -1,1 +1,24 @@
-main = putStrLn "hello, world"
+--main = putStrLn "hello, world"
+
+-- main = do
+--   putStrLn "Hello, what's your name?"
+--   name <- getLine
+--   putStrLn ("Hey " ++ name ++ ", you rock!")
+
+-- main = do
+--   putStrln "Hello, what's your name?"
+--   name <- getLine
+--   putStrLn $ "Zis is your future: " ++ tellFortune name
+  
+import Data.Char
+
+main = do
+  putStrLn "What's your first name?"
+  firstName <- getLine
+  putStrLn "What's your last name?"
+  lastName <- getLine
+  let bigFirstName = map toUpper firstName
+      bigLastName = map toUpper lastName
+  putStrLn $ "hey " ++ bigFirstName ++ " "
+                    ++ bigLastName++ " "
+                    ++ ", how are you?"
